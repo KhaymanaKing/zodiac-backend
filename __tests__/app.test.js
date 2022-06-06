b1102a9654ac5f8b7e0477c0d5646dc638202aeb
Khayman King <khaymanak@gmail.com>
2022-06-06 16:04:29 -0700

    both tests are passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,6 +17,17 @@ describe('backend-zodiac routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('/zodiacs/:id should return zodiacs detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = { 
+      'id':'1', 
+      'name':'aquarius', 
+      'dates':'Jan 21 - Feb 19', 
+      'symbol':'Water Bearer' 
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+
   afterAll(() => {
     pool.end();
   });
